9ee4e34d13925d495d9ae2899e9a22dc8c9360dc
sasfan1988 <sasfan.a.wella@gmail.com>
2022-04-06 20:52:21 +0700

    Update for 2022

---
+++ js/formatPub2020.js
@@ -0,0 +1,150 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(xhttp.responseText);
+      var pubJournals = response.listJournals;
+      var pubProceeds = response.listProceeds;
+      var pubPreprint = response.listPreprint;
+
+      var staffId = [
+        "ahma080", "choi002", "eddw001", "edis008", "fere001",
+        "gagu001", "msho001", "muha207", "muza002", "nady003",
+        "roni012", "sasf001", "wahy050", "wili001", "yano001"
+      ]
+
+      var fullname = [
+        "A. R. T. Nugraha", "C. N. Rangkuti", "E. H. Hasdeo",
+        "E. Suprayoga", "F. Oemry", "G. K. Sunnardianto",
+        "M. S. Ukhtary", "M. Y. Hanna", "M. P. M. Akhir",
+        "N. Amalia", "R. Muslim", "S. A. Wella",
+        "W. D. Saputri", "W. J. Sari", "Y. P. Sarwono"
+      ]
+
+      var out1 = '';
+      var authors;
+      for (var i = 0; i < pubJournals.length; i++) {
+
+        var temp = [];
+
+        for (var j = 0; j < pubJournals[i].author.length; j++) {
+          temp[j] = pubJournals[i].author[j];
+          for (var k = 0; k < fullname.length; k++) {
+            if (pubJournals[i].author[j]==fullname[k]) {
+              temp[j] = "<a href=../team/staff/"+staffId[k]+".html> "+fullname[k]+"</a>";
+              break;
+            }
+          }
+        }
+
+        authors = '';
+        if (temp.length==1) {
+          authors += temp[0];
+        } else if (temp.length==2) {
+          authors += temp[0]+' and '+temp[1];
+        } else {
+          for (var j = 0; j < temp.length-1; j++) {
+            authors += temp[j]+', ';
+          }
+          authors += 'and '+ temp[temp.length-1];
+        }
+
+        out1 += '<li>';
+        out1 += authors+', ';
+        out1 += '<i>"'+pubJournals[i].title+'"</i>'+', ';
+        out1 += '<a href='+pubJournals[i].doi+'>';
+        out1 += pubJournals[i].journal+' ';
+        out1 += '<b>'+pubJournals[i].volume+'</b>'+', ';
+        out1 += pubJournals[i].pages+', ';
+        out1 += '('+pubJournals[i].year+').'
+        out1 += '</a></li>';
+
+      }
+
+      document.getElementById('pubJournals').innerHTML = out1;
+
+      var out2 = '';
+      for (var i = 0; i < pubProceeds.length; i++) {
+
+        var temp = [];
+
+        for (var j = 0; j < pubProceeds[i].author.length; j++) {
+          temp[j] = pubProceeds[i].author[j];
+          for (var k = 0; k < fullname.length; k++) {
+            if (pubProceeds[i].author[j]==fullname[k]) {
+              temp[j] = "<a href=../team/staff/"+staffId[k]+".html> "+fullname[k]+"</a>";
+              break;
+            }
+          }
+        }
+
+        authors = '';
+        if (temp.length==1) {
+          authors += temp[0];
+        } else if (temp.length==2) {
+          authors += temp[0]+' and '+temp[1];
+        } else {
+          for (var j = 0; j < temp.length-1; j++) {
+            authors += temp[j]+', ';
+          }
+          authors += 'and '+ temp[temp.length-1];
+        }
+
+        out2 += '<li>';
+        out2 += authors+', ';
+        out2 += '<i>"'+pubProceeds[i].title+'"</i>'+', ';
+        out2 += '<a href='+pubProceeds[i].doi+'>';
+        out2 += pubProceeds[i].journal+' ';
+        out2 += '<b>'+pubProceeds[i].volume+'</b>'+', ';
+        out2 += pubProceeds[i].pages+', ';
+        out2 += '('+pubProceeds[i].year+').'
+        out2 += '</a></li>';
+
+      }
+
+      document.getElementById('pubProceeds').innerHTML = out2;
+
+      var out3 = '';
+      for (var i = 0; i < pubPreprint.length; i++) {
+
+        var temp = [];
+
+        for (var j = 0; j < pubPreprint[i].author.length; j++) {
+          temp[j] = pubPreprint[i].author[j];
+          for (var k = 0; k < fullname.length; k++) {
+            if (pubPreprint[i].author[j]==fullname[k]) {
+              temp[j] = "<a href=../team/staff/"+staffId[k]+".html> "+fullname[k]+"</a>";
+              break;
+            }
+          }
+        }
+
+        authors = '';
+        if (temp.length==1) {
+          authors += temp[0];
+        } else if (temp.length==2) {
+          authors += temp[0]+' and '+temp[1];
+        } else {
+          for (var j = 0; j < temp.length-1; j++) {
+            authors += temp[j]+', ';
+          }
+          authors += 'and '+ temp[temp.length-1];
+        }
+
+        out3 += '<li>';
+        out3 += authors+', ';
+        out3 += '<i>"'+pubPreprint[i].title+'"</i>'+', ';
+        out3 += '<a href='+pubPreprint[i].doi+'>';
+        out3 += pubPreprint[i].journal+' ';
+        out3 += '<b>'+pubPreprint[i].volume+'</b>'+', ';
+        out3 += pubPreprint[i].pages+', ';
+        out3 += '('+pubPreprint[i].year+').'
+        out3 += '</a></li>';
+
+      }
+
+      document.getElementById('pubPreprint').innerHTML = out3;
+
+    }
+};
+xhttp.open("GET", "./pub/pub2020.json", true);
+xhttp.send();
---
+++ js/navLinks.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu(){
+  navLinks.style.right="0px";
+}
+function hideMenu(){
+  navLinks.style.right="-300px";
+}
+
+//w3IncludeHTML();
